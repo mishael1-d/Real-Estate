55f61e67aa7d1028344a5995ba1656ff578b0c57
Mishael <Mishaeldada@gmail.com>
2021-10-31 16:09:51 +0100

    changed the header background, also add images for the project files.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,16 +1,29 @@
 import React from "react";
 import InputField from './Input-field'
+import { data } from "../data";
 
 const Content = () => {
   return (
     <div className="content container">
-      <div className="text_content">
-        <h2>We're The Next Generation Of Real Estate Agents.</h2>
-        <p>
-          We specialize in providing these services buying, selling and renting
-          of apartments.
-        </p>
-      </div>
+        <div className="image-container">
+          
+          <div className="text_content">
+            <h2>We're The Next Generation Of Real Estate Agents.</h2>
+            <p>
+              Discover a place you'll love to live
+            </p>
+            <div className="search-container">
+            <input type="search" name="" id="" placeholder="Location, City, Area Code"/>
+            <button type="submit"></button>
+            </div>
+          </div>
+            {/* {data.map((image, index)=>{
+              return (
+                <span key={index}><img src={image} alt="" /></span>
+              )
+            })} */}
+            {/* <img src={data[0]} alt="" srcset="" /> */}
+        </div>
       <InputField/>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,12 @@
+import image1 from './buildings/image1.JPG'
+import image2 from './buildings/image2.JPG'
+import image3 from './buildings/image3.JPG'
+import image4 from './buildings/image4.JPG'
+import image5 from './buildings/image5.JPG'
+import image6 from './buildings/image6.JPG'
+import image7 from './buildings/image7.JPG'
+
+export const data = [image1,image2,image3,image4,image5,image6,image7]
+
+
+
